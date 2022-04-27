42faba727e7acb42edd1ccc28433d8a7d8f949bd
Mahdi Sadegh-zadeh <mdpro.smm@gmail.com>
2022-04-27 21:01:08 +0430

    default theming

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,4 +1,6 @@
+import { createTheme, ThemeProvider } from "@mui/material";
 import "assets/icons/fontawesome6.1.1-pro/css/all.min.css";
+import "assets/sass/loader.scss";
 import "kernel/prototypes/array";
 import "kernel/prototypes/number";
 import "kernel/prototypes/object";
@@ -8,15 +10,28 @@ import AuthLayout from "layouts/Auth.layout";
 import DashboardLayout from "layouts/Dashboard.layout";
 import SiteLayout from "layouts/Site.layout";
 import { NextComponentType } from "next";
-import React, { Component, ElementType, Fragment } from 'react';
+import React, { Component, ElementType } from 'react';
 import { wrapper } from '../kernel/redux/storage';
-
 
 interface AppProps extends RouterProps {
     pageProps: any,
     Component: NextComponentType
     err: any
 }
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#6200E8',
+        }
+    },
+    typography: {
+        fontFamily: "Roboto",
+        button: {
+            textTransform: 'none',
+
+        }
+    }
+});
 
 class App extends Component<AppProps> {
 
@@ -37,18 +52,15 @@ class App extends Component<AppProps> {
     }
 
     public render() {
-        console.log(this.props.router.pathname.startsWith('/dashboard'))
+        const { Component, pageProps, ...props } = this.props;
         const Layout = this.getLayout();
 
-        console.log(Layout)
-
-        const { Component, pageProps, ...props } = this.props;
         return (
-            <Fragment>
+            <ThemeProvider theme={theme}>
                 <Layout>
                     <Component {...pageProps} {...props} />
                 </Layout>
-            </Fragment>
+            </ThemeProvider>
         )
     }
 }
